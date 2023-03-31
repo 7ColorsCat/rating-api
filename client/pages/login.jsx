3e3082845a81561e3cd1cc8b95081c59a7c632b4
katinbox <tranquanki28@gmail.com>
2023-03-31 11:29:11 +0700

    authenticated

---
+++ client/pages/login.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log({ email, password })
+    };
+
+    return (
+        <Box maxW={"container.md"}
+            shadow={'lg'}
+            border={'1px'}
+            borderColor={'whiteAlpha.500'}
+            rounded={'xl'}
+        >
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+            >
+                <Box mx="auto" bg="transparent" p="8" borderRadius="md">
+                    <form onSubmit={handleSubmit}>
+                        <FormControl id="email" isRequired>
+                            <FormLabel>Username</FormLabel>
+                            <Input
+                                type="text"
+                                placeholder="Enter your username"
+                                value={email}
+                                onChange={(event) =>
+                                    setEmail(event.target.value)
+                                }
+                                variant={"filled"}
+                            />
+                        </FormControl>
+                        <FormControl id="password" mt="4" isRequired>
+                            <FormLabel>Password</FormLabel>
+                            <Input
+                                type="password"
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(event) =>
+                                    setPassword(event.target.value)
+                                }
+                                variant={"filled"}
+                            />
+                        </FormControl>
+                        <Button
+                            type="submit"
+                            colorScheme="gray"
+                            mt="4"
+                            w="full"
+                            size="lg"
+                            disabled={!email || !password}
+                        >
+                            Sign in
+                        </Button>
+                    </form>
+                </Box>
+            </motion.div>
+        </Box>
+    );
+};
+
+export default Login;
